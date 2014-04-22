2d8b23dcc99440785afc580f7b01960ad86a2e97
Xinghu <xinghu@lhtekiMacBook-Pro.local>
2014-04-22 00:29:27 -0400

    2014.4.22

---
+++ src/LargestRectangleHistogram.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+/**
+ * 
+ */
+
+/**
+ * @author xinghu
+ *
+ */
+public class LargestRectangleHistogram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] height = {2,1,5,6,2,3};
+		System.out.println(largestRectangleArea(height));
+	}
+	
+	public static int largestRectangleArea(int[] height) {
+		int max = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		return max;
+	}
+}
