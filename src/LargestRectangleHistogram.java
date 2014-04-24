58235bbe9dfbbbcfa05587335a9411c423ee7004
Xinghu <xinghu@lhtekiMacBook-Pro.local>
2014-04-23 21:45:17 -0400

    2014.4.23

--- src/LargestRectangleHistogram.java
+++ src/LargestRectangleHistogram.java
@@ -20,8 +20,37 @@ public class LargestRectangleHistogram {
 	}
 	
 	public static int largestRectangleArea(int[] height) {
-		int max = 0;
-		Stack<Integer> stack = new Stack<Integer>();
+		int max = 0, w = 0, h;
+		if (height.length == 0)
+			return 0;
+		Stack<Integer> heightStack = new Stack<Integer>();
+		Stack<Integer> indexStack = new Stack<Integer>();
+		
+		for (int i = 0; i < height.length; i ++)
+		{
+			if (heightStack.isEmpty() || height[i] > heightStack.peek()) {
+				heightStack.push(height[i]);
+				indexStack.push(i);
+			}
+			else if (height[i] < heightStack.peek()) {
+				
+				while (!heightStack.isEmpty() && height[i] < heightStack.peek()) 
+				{
+					w = i - indexStack.pop();
+					h = heightStack.pop();
+					max = Math.max(max, w * h);
+				}
+				heightStack.push(height[i]);
+				indexStack.push(i - w);
+			}
+		}
+		
+		while (!heightStack.isEmpty())
+		{
+			h = heightStack.pop();
+			w = height.length - indexStack.pop();
+			max = Math.max(max, w * h);
+		}
 		return max;
 	}
 }
